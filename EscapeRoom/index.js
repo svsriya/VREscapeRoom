a138856064b55da574ff3037b419a09d4613f2d1
Sriya Vudata <svsriya@gmail.com>
2020-05-02 00:46:25 -0400

    added desks

--- EscapeRoom/index.js
+++ EscapeRoom/index.js
@@ -1,7 +1,6 @@
 
 var scene, camera, renderer;
 var controls;
-var interactObjs = [];
 var raycaster;
 var moveForward = false;
 var moveBack = false;
@@ -9,7 +8,8 @@ var moveRight = false;
 var moveLeft = false;
 var mouse = new THREE.Vector2();
 // interactive objects
-var doorKey, symbolBook, table;
+var interactObjs = [];
+var doorKey, symbolBook;
 var door;
 // variables for objects the user picks up
 var pickupable = [];
@@ -114,6 +114,7 @@ window.onload = function init()
       case 68:  // D key pressed, dropping object
         if(pickedUp)
         {
+          // set object to its original location 
           camera.remove(pickedUpObject);
           pickedUpObject.position.set(objOgLocation.x, objOgLocation.y, objOgLocation.z);
           scene.add(pickedUpObject);
@@ -146,46 +147,54 @@ window.onload = function init()
 
   // create the empty room
   // floor
-  var floorGeometry = new THREE.CubeGeometry(15, 1, 20);
+  var floorGeometry = new THREE.CubeGeometry(30, 1, 40);
   var floorMaterial = new THREE.MeshBasicMaterial({ color: 0x6f3610, wireframe: false });
   var floorCube = new THREE.Mesh(floorGeometry, floorMaterial);
   floorCube.position.y = -5;
   scene.add( floorCube );
 
   // ceiling
-  var ceilingGeometry = new THREE.CubeGeometry(15, 1, 20);
+  var ceilingGeometry = new THREE.CubeGeometry(30, 1, 40);
   var ceilingMaterial = new THREE.MeshBasicMaterial({ color: 0xdfdfdf, wireframe: false });
   var ceilingCube = new THREE.Mesh(ceilingGeometry, ceilingMaterial);
   ceilingCube.position.y = 5;
   scene.add( ceilingCube );
 
   // left wall
-  var leftWallGeometry = new THREE.CubeGeometry(1, 10, 20);
+  var leftWallGeometry = new THREE.CubeGeometry(1, 10, 40);
   var leftWallMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
   var leftWallCube = new THREE.Mesh(leftWallGeometry, leftWallMaterial);
-  leftWallCube.position.x = -8;
+  leftWallCube.position.x = -15;
   scene.add( leftWallCube );
 
   // right wall
-  var rightWallGeometry = new THREE.CubeGeometry(1, 10, 20);
+  var rightWallGeometry = new THREE.CubeGeometry(1, 10, 40);
   var rightWallMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
   var rightWallCube = new THREE.Mesh(rightWallGeometry, rightWallMaterial);
-  rightWallCube.position.x = 8;
+  rightWallCube.position.x = 15;
   scene.add( rightWallCube );
 
   // front wall
-  var frontWallGeometry = new THREE.CubeGeometry(15, 10, 1);
+  var frontWallGeometry = new THREE.CubeGeometry(30, 10, 1);
   var frontWallMaterial = new THREE.MeshBasicMaterial({ color: 0xfdfdfd, wireframe: false });
   var frontWallCube = new THREE.Mesh(frontWallGeometry, frontWallMaterial);
-  frontWallCube.position.z = -10;
+  frontWallCube.position.z = -20;
   scene.add( frontWallCube );
 
   // back wall
-  var backWallGeometry = new THREE.CubeGeometry(15, 10, 1);
+  var backWallGeometry = new THREE.CubeGeometry(30, 10, 1);
   var backWallMaterial = new THREE.MeshBasicMaterial({ color: 0xfdfdfd, wireframe: false });
   var backWallCube = new THREE.Mesh(backWallGeometry, backWallMaterial);
-  backWallCube.position.z = 10;
+  backWallCube.position.z = 20;
   scene.add( backWallCube );
+
+  // lighting
+  var ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+  scene.add(ambientLight);
+
+  var spotLight = new THREE.SpotLight( 0xffffff, 10 );
+  spotLight.position.set(0, 5, 0);
+  scene.add(spotLight);
 
   // symbols painting
   var loader = new THREE.GLTFLoader();
@@ -193,23 +202,50 @@ window.onload = function init()
     var symbolPainting = new THREE.Object3D();
     symbolPainting = gltf.scene;
     symbolPainting.scale.set(0.1,0.1,0.1);
-    symbolPainting.position.set(-7, 0, 0);
+    symbolPainting.position.set(-14.25, 0, 0);
     scene.add(symbolPainting);
-  })
+  });
 
-  // lighting
-  var ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-  scene.add(ambientLight);
+  // desks
+  var desks = new THREE.Object3D();
+  loader.load('./models/computerdesk/scene.gltf', function(gltf){
+    var desk = new THREE.Object3D();
+    desk = gltf.scene;
+    desk.scale.set(0.02,0.02,0.02);
+    for(var i = 0; i < 4; i++)
+    {
+      var cloneDesk = desk.clone();
+      cloneDesk.position.set(4-i*4.5,-4.5,18.8);
+      desks.add(cloneDesk);
+    }
+  });
+  scene.add(desks);
 
-  var spotLight = new THREE.SpotLight( 0xff45f6, 10 );
-  spotLight.position.set(0, 5, 0);
-  scene.add(spotLight);
+  // desk chairs
+  var chairs = new THREE.Object3D();
+  loader.load('./models/deskchair/scene.gltf', function(gltf){
+    var chair = new THREE.Object3D();
+    chair = gltf.scene;
+    chair.scale.set(0.08,0.08,0.08);
+    for( var i = 0; i < 4; i++ )
+    {
+      var cloneChair = chair.clone();
+      cloneChair.position.set(4-i*4.5, -1.7, 18);
+      // orient chair correctly
+      var newDir = new THREE.Vector3(-1,0,0);
+      var pos = new THREE.Vector3();
+      pos.addVectors(newDir, cloneChair.position);
+      cloneChair.lookAt(pos);
+      chairs.add(cloneChair);
+    }
+  });
+  scene.add(chairs);
 
   //door
   var doorGeometry = new THREE.CubeGeometry(3, 7, 1);
   var doorMaterial = new THREE.MeshBasicMaterial({ color: 0xcf824e });
   door = new THREE.Mesh(doorGeometry, doorMaterial);
-  door.position.set(0, -1, -9.75);
+  door.position.set(0, -1, -19.75);
   scene.add(door);
 
   // temp key
@@ -222,12 +258,11 @@ window.onload = function init()
   pickupable.push(doorKey);
 
   // symbolBook
-  loader = new THREE.GLTFLoader();
   loader.load('./models/book/scene.gltf', function(gltf){
       symbolBook = new THREE.Object3D();
       symbolBook = gltf.scene;
       symbolBook.scale.set(0.5,0.5,0.5);
-      symbolBook.position.set(0, -2, 0);
+      symbolBook.position.set(10, -2, 0);
       scene.add(symbolBook);
       interactObjs.push(symbolBook);
   });
@@ -235,6 +270,7 @@ window.onload = function init()
   GameLoop();
 }
 
+// checks if object is in list
 function containsObj( obj, list )
 {
   for( var i = 0; i < list.length; i++ )
@@ -279,18 +315,22 @@ function update()
 		} else {
       console.log('intersects length: ' + intersects.length);
 			for ( var i = 0; i < intersects.length; i++ ) {
-
+        // check if the object in the raycaster is pickupable, and if so pick it up
+        // TODO make sure user can't try to pick up something when already holding something
         if( containsObj(intersects[i].object, pickupable) )
         {
+          // save the original location of object so user drops in the original place if they want to drop an object
           objOgLocation.x = intersects[i].object.position.x;
           objOgLocation.y = intersects[i].object.position.y;
           objOgLocation.z = intersects[i].object.position.z;
   				// intersects[i].object.position.y = camera.position.y;
+          // add to camera to simulate picking up
   				camera.add(intersects[i].object);
   				intersects[i].object.position.set(2,-2,-5);
           pickedUpObject = intersects[i].object;
           pickedUp = true;
         }
+        // temp for checking interaction with book
         else( intersects[i].object.position == symbolBook.position )
         {
           console.log("clicked on book");
